85895a63682bc0ddb0f68b1ce3537da431b24ce9
William Chabla <w_chabla@hotmail.com>
2018-06-23 00:29:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 def dictionary
+  {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_arry = tweet.split(" ")
+  tweet_new_array = []
+  tweet_new = ""
+  
+  replace_dict = dictionary
+  
+  tweet_arry.each do |word|
+    replace_dict.each do |word_replace, replacement|
+      
+
